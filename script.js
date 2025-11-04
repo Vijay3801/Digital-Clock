336393ddc45fc61c768af1cb59aecc8779137a7e
Vihay3801 <thakurvijay3182@gmail.com>
2025-11-04 14:48:18 +0530

    changed the time format to 12 hour

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const time = document.querySelector("#time")
 
 setInterval(()=>{
     let currentDateTime = new Date();
-    time.innerHTML=currentDateTime.toLocaleTimeString();
+    time.innerHTML=currentDateTime.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true  // <-- this enables 12-hour format
+    });
     date.innerHTML=currentDateTime.toLocaleDateString();
 },1000)
